210ef8c98d2e8a2f5342a33cd44d62594a03dc13
zmflqhaos <zmflqhaos123@gmail.com>
2023-07-09 23:47:19 +0900

    뚝딱뚝딱 틀짜놓기

---
+++ Assets/Scripts/SingleNPool/PoolItem.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class PoolItem
+{
+    public string name;
+    public GameObject gameObject;
+    public int defaultCount;
+}
