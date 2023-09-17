59577d92a31520ceedebe74c1b6a944e838f3088
zmflqhaos <zmflqhaos123@gmail.com>
2023-09-18 00:11:18 +0900

    브금 사운드 약간 조절

--- Assets/Scripts/newGameMode/newCommandManager.cs
+++ Assets/Scripts/newGameMode/newCommandManager.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class newCommandManager : CommandManager
 {
     private bool isBuging;
     private int point;
     private int difficultValue;
+    [SerializeField] private TextMeshProUGUI devPer;
     [SerializeField] private Transform arrowTrs;
+    [SerializeField] private GameObject bugImage;
 
     protected override void Start()
     {
@@ -39,6 +42,7 @@ public class newCommandManager : CommandManager
         {
             isBuging = false;
             commandTrs.transform.localScale = arrowTrs.localScale = new Vector3(1, 1, 1);
+            bugImage.SetActive(false);
         }
     }
 
@@ -48,10 +52,11 @@ public class newCommandManager : CommandManager
         {
             if (!isBuging)
             {
+                devPer.color = Color.white;
                 if (Random.Range(1, 101) > 70 - difficultValue)
                 {
                     isBuging = true;
-                    int rand = Random.Range(1, 4);
+                    int rand = Random.Range(1, 6);
 
                     Invoke("RandBug" + rand, 0);
                     point = devPersent;
@@ -59,7 +64,8 @@ public class newCommandManager : CommandManager
             }
             else
             {
-                ChangeDevPersent(-2);
+                devPer.color = Color.red;
+                ChangeDevPersent(-1);
             }
             yield return new WaitForSeconds(1.5f);
         }
@@ -80,6 +86,18 @@ public class newCommandManager : CommandManager
         commandTrs.transform.localScale = arrowTrs.localScale = new Vector3(0.5f, 0.5f, 1);
     }
 
+    private void RandBug4()
+    {
+        commandTrs.transform.localScale = arrowTrs.localScale = new Vector3(-1, 1, 1);
+    }
+
+    private void RandBug5()
+    {
+        //무슨 버그를 넣을까
+        bugImage.SetActive(true);
+    }
+
+
     private void SetDifficultValue()
     {
         switch (GameManager.difficult)
@@ -91,19 +109,19 @@ public class newCommandManager : CommandManager
                 difficultValue = 0;
                 break;
             case Difficult.Easy:
-                difficultValue = 5;
+                difficultValue = 3;
                 break;
             case Difficult.Normal:
-                difficultValue = 10;
+                difficultValue = 6;
                 break;
             case Difficult.Hard:
-                difficultValue = 15;
+                difficultValue = 9;
                 break;
             case Difficult.Very_Hard:
-                difficultValue = 20;
+                difficultValue = 12;
                 break;
             case Difficult.DeadLine:
-                difficultValue = 25;
+                difficultValue = 15;
                 break;
         }
     }
